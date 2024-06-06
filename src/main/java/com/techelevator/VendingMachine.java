fbdb296afb6aacad4c7e4c2a69e0cbb223e3a754
Bryan Freedman <contact@bryanfreedman.com>
2024-06-06 11:59:54 -0400

    11:59AM 6/6/2024 built out csv reader and splitter to put into Map

---
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -0,0 +1,39 @@
+package com.techelevator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class VendingMachine {
+
+    public void displayItems(){
+
+    }
+
+
+    public Map<String, Product> inventory = new HashMap<>();
+
+
+    public void loadInventory() throws IOException {
+        File inputFile = new File("vendingmachine.csv");
+        Scanner inputScanner = new Scanner(inputFile);
+        while (inputScanner.hasNextLine()){
+            String line = inputScanner.nextLine();
+            String[] elements = line.split("\\|");
+            String slot = elements[0];
+            String name = elements[1];
+            Double price = Double.parseDouble(elements[2]);
+            String type = elements[3];
+
+            inventory.put(slot, new Product(slot, name, price, type));
+        }
+
+    }
+
+
+
+
+
+}
