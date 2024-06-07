5ba7469c649a060b6c9b5f1706c8fcee68cef4f6
David Hall <theonetruedavidhall@gmail.com>
2024-06-07 15:28:27 -0500

    Finished Logger with great happy :) .

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,9 +1,10 @@
 package com.techelevator;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.*;
 
 public class VendingMachine {
@@ -44,6 +45,12 @@ public class VendingMachine {
     }
     public void feedMoney(BigDecimal amount) {
         balance = balance.add(amount);
+        String activity = String.format("FEED MONEY: $%s $%s", amount, balance);
+        try {
+            logger(activity);
+        } catch (IOException e) {
+            System.err.println("Error writing to log..." + e.getMessage());
+        }
     }
 
     public BigDecimal getBalance() {
@@ -52,6 +59,8 @@ public class VendingMachine {
 
     public void purchaseProduct(String expectedSlot){
         Product product = inventory.get(expectedSlot);
+        String activity = "";
+
         if (inventory.containsKey(expectedSlot)) {
             if (product.getQuantity() == 0) {
                 System.out.println();
@@ -74,7 +83,12 @@ public class VendingMachine {
                 System.out.format("Thank you for your purchase of: %s for $%.2f!",product.getName(),product.getPrice());
                 System.out.println("\n\nBalance remaining: $" + getBalance());
                 //return to purchase menu
-
+                activity = String.format("%s %s $%.2f $%.2f",product.getName(), product.getSlot(), product.getPrice(), getBalance());
+                try {
+                    logger(activity);
+                } catch (IOException e) {
+                    System.err.println("Error writing to log..." + e.getMessage());
+                }
             }
         } else {
             System.out.println("Please check your option.");
@@ -85,6 +99,8 @@ public class VendingMachine {
         int quarterCount = 0;
         int dimeCount = 0;
         int nickleCount = 0;
+        BigDecimal change = balance;
+        String activity = "";
         while (getBalance().compareTo(BigDecimal.valueOf(0.25)) >= 0) {
             balance = balance.subtract(BigDecimal.valueOf(0.25));
             quarterCount++;
@@ -98,8 +114,12 @@ public class VendingMachine {
             nickleCount++;
         }
         System.out.println("Here is your change: " + quarterCount + " quarters, " + dimeCount + " dimes, " + nickleCount + " nickles.");
-        //balance = 0 giving change
-        //returns to main menu
+        activity = String.format("GIVE CHANGE: $%.2f $%.2f", change, balance);
+        try {
+            logger(activity);
+        } catch (IOException e) {
+            System.err.println("Error writing to log..." + e.getMessage());
+        }
     }
 
     public void getMessage(Product product){
@@ -107,8 +127,39 @@ public class VendingMachine {
 
         if(type.equals("Chip") || type.equals("chip")){
             System.out.println("Crunch Crunch, It's Yummy!");
+        } else if (type.equals("Candy") || type.equals("candy")) {
+            System.out.println("Munch Munch, Mmm Mmm Good!");
+        } else if (type.equals("Drink") || type.equals("drink")) {
+            System.out.println("Glug Glug, Chug Chug!");
+        } else if (type.equals("Gum") || type.equals("gum")) {
+            System.out.println("Chew Chew, Pop!");
+        } else {
+            System.out.println("Invalid type within csv file.");
         }
 
     }
+    public void logger(String activity) throws IOException {
+        File log = new File("Log.txt");
+        PrintWriter logWriter = null;
+    try {
+
+        if (log.exists()) {
+            logWriter = new PrintWriter(new FileWriter(log, true));
+        } else {
+            logWriter = new PrintWriter(log);
+        }
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        String timeStamp = String.format("%tm/%td/%tY %tI:%tM:%tS %Tp", date, date, date, time, time, time, time);
+
+        logWriter.append(timeStamp + " " + activity + "\n");
+
+    } catch (IOException e) {
+        System.err.println("Error writing to file..." + e.getMessage());
+    }
+        logWriter.flush();
+        logWriter.close();
+    }
+
 }
 
